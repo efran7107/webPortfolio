095b5bf637dc80f05c3f8d5f15b3631552f0eee1
efran7107 <efran7107@gmail.com>
2024-11-19 10:58:56 -0700

    adding objects

---
+++ src/assets/sites/sites.js
@@ -0,0 +1,22 @@
+const sites = () => {
+  return [
+    {
+      name: "Simple js Tic Tac Toe",
+      site: "https://tic-tac-toe-sandy-seven.vercel.app/",
+      github: "https://github.com/efran7107/TicTacToe/tree/main",
+      description:
+        "A simple js tic tac toe game allowing ties and making it tough to win.",
+      img: "../assets/site-screenshots/simpleJsTTT.png",
+    },
+    {
+      name: "Vite React TicTacToe app",
+      site: "https://tic-tac-toe-vite-app-eight.vercel.app/",
+      github: "https://github.com/efran7107/TicTacToeViteApp/tree/main",
+      description:
+        "A continuous tic tac toe game that only ends with a winner. Developed with vite and react. Has the ability to be installed on android devices. Only available on the web for now read the README.md in github for more details.",
+      img: "../assets/site-screenshots/reactAppTTT.png",
+    },
+  ];
+};
+
+export default sites();
---
+++ src/js/sites.js
@@ -0,0 +1,8 @@
+import "../assets/sites/sites.js";
+
+const siteCont = document.createElement("div").classList.add("site-cont");
+
+// const h1Tag = (document.createElement("h1").innerHTML = site.name);
+// h1Tag.textContent = site.name;
+
+console.log(sites.sites);
